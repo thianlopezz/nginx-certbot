437af78f4f9ee9f5df51f123df999a1f4440cec0
Thian Lopez <thianlopezz@gmail.com>
2020-03-06 17:27:56 -0500

    nueva prueba

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(chaskiy.com)
+domains=(chaskiy.com,www.chaskiy.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="thianlopezz@gmail.com" # Adding a valid address is strongly recommended
