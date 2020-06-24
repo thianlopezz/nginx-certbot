3574f5c24cb88ca2966233ad51154d3571b663d4
Thian Lopez Z <thianlopezz@gmail.com>
2020-06-24 01:21:17 -0500

    preubas

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,12 +5,12 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-mainDomain=(go.hispanadeseguros.com)
-domains=(go.hispanadeseguros.com)
+mainDomain=(hispana.bitbox.com)
+domains=(hispana.bitbox.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="thianlopezz@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $mainDomain. Continue and replace existing certificate? (y/N) " decision
